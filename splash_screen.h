389d801d5a098c67e182629473ac84dd4cf2c68c
ahnafahmed09 <132055191+ahnafahmed09@users.noreply.github.com>
2023-06-18 16:51:48 +0600

    splash_screen.h
    blinking file

---
+++ splash_screen.h
@@ -0,0 +1,48 @@
+
+void splash_screen(void)
+{
+
+
+
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("\n");
+printf("              888    888          888            888      888b     d888                                                                              888    \n");
+printf("              888    888          888            888      8888b   d8888                                                                              888    \n");
+printf("              888    888          888            888      88888b.d88888                                                                              888    \n");
+printf("              8888888888  .d88b.  888888 .d88b.  888      888Y88888P888  8888b.  88888b.   8888b.   .d88b.   .d88b.  88888b.d88b.   .d88b.  88888b.  888888 \n");
+printf("              888    888 d88' '88 888   d8P  Y8b 888      888 Y888P 888     '88b 888 '88b     '88b d88P'88b d8P  Y8b 888  888  88b d8P  Y8b 888  88b 888    \n");
+printf("              888    888 888  888 888   88888888 888      888  Y8P  888 .d888888 888  888 .d888888 888  888 88888888 888  888  888 88888888 888  888 888    \n");
+printf("              888    888 Y88..88P Y88b. Y8b.     888      888   '   888 888  888 888  888 888  888 Y88b 888 Y8b.     888  888  888 Y8b.     888  888 Y88b.  \n");
+printf("              888    888  'Y88P'   'Y888 'Y8888  888      888       888 'Y888888 888  888 'Y888888  'Y88888  'Y8888  888  888  888  'Y8888  888  888  'Y888 \n");
+printf("                                                                                                        888                                                  \n");
+printf("                                                                                                   Y8b d88P                                                  \n");
+printf("                                                                                                    'Y88P'                                                   \n");
+Sleep(1000);
+system("cls");
+
+
+}
