f308fb7eb11076191832a918f525e7b8df9562f0
ahnafahmed09 <132055191+ahnafahmed09@users.noreply.github.com>
2023-06-25 03:20:04 +0600

    Update room.h

--- room.h
+++ room.h
@@ -1,17 +1,38 @@
 #include <stdio.h>
-
+#include <string.h>
 
 
 
 void room(void)
 {
-    int r;
-    printf("\nPress 1 for VIP room : \n");
+    int r,s;
+
+    printf("Enter any number to show rooms \n");
+    printf("Enter -1 to exit room show and make a choice \n");
+    scanf("%d",&s);
+
+    //Menu Driven Program
+    while(s!=-1)
+    {
+      printf("\nPress 1 for VIP room : \n");
     printf("\nPress 2 for Business class room : \n");
     printf("\nPress 3 for Economy class room : \n");
+
+    printf("\n\n\nEnter any number to show rooms \n");
+    printf("\n\n\nEnter -1 to exit room show and make a choice \n");
+    scanf("%d",&s);
+    }
+
+    printf("\n\n\n\n        _______________________________________\n");
+    printf("        |             ENTER YOUR CHOICE       |\n");
+    printf("        |_____________________________________|\n");
+
     scanf("%d",&r);
 
-    if(r==1)
+
+
+
+      if(r==1)
     {
         slot_vip();
     }
@@ -24,115 +45,232 @@ void room(void)
         slot_economy();
     }
 
+
+
 }
 
 void slot_vip(void)
 {
-    int number;
-   room_diagram();
-   printf("Enter the room number you want to book :");
-   scanf("%d",&number);
+    int room_no;
+
+
+
+
+    printf("\n\n\n\n        ___________________________________________\n");
+    printf("        |             ENTER ROOM NUMBER          |\n");
+    printf("        |________________________________________|\n");
+    scanf("%d", &room_no);
+
+
+    FILE *file = fopen("roomsvip.txt","r");
+    int roomExists = 0;
+
+
+
+    if (file != NULL) {
+        int given_room_no;
+
+        while (fscanf(file,"%d",&given_room_no) == 1) {
+            if (given_room_no == room_no) {
+                roomExists = 1;
+                break;
+            }
+        }
+
+        fclose(file);
+    }
+
+    while (roomExists) {
+
+    printf("\n\n\n\n        ___________________________________\n");
+    printf("        |Room %d has already been booked. |\n",room_no);
+    printf("        |Enter a different room number     |\n");
+    printf("        |__________________________________|\n");
+        scanf("%d", &room_no);
+
+        file = fopen("roomsvip.txt", "r");
+        roomExists = 0;
+
+        if (file != NULL) {
+            int given_room_no;
+
+            while (fscanf(file, "%d",&given_room_no) == 1) {
+                if (given_room_no == room_no) {
+                    roomExists = 1;
+                    break;
+                }
+            }
+
+            fclose(file);
+        }
+    }
+
+    file = fopen("roomsvip.txt", "a");
+
+    if (file == NULL) {
+        printf("Failed to open file.\n");
+        return 0;
+    }
+
+    fprintf(file, "%d\n", room_no);
+    fclose(file);
+
+     printf("\n\n\n\n        _______________________________________\n");
+    printf("        |Room %d has been successfully booked |\n",room_no);
+    printf("        |_____________________________________|\n");
+
+    Sleep(10000);
+    system("cls");
 }
 
 void slot_business(void)
 {
-    room_diagram();
-    char rooms[3],roomfile[3];
-
-    FILE *room;
-    room=fopen("room.txt","r");
-
-
-        //storing the username and password from file
-        fscanf(room,"%s", roomfile);
-
-        while(1)
-        {
-
-    printf("\n\n\n\n        _______________________________________\n");
-    printf("        |             Room Booking            |\n");
-    printf("        |_____________________________________|\n");
-
-    printf("\n\n\n\n        _______________________________________\n");
-    printf("        |        Enter room number :          |\n");
-    printf("        |_____________________________________|\n");
-
-
-          fflush(stdin);
-          fgets(rooms,sizeof(rooms),stdin);
-        rooms[strlen(rooms)-1]='\0'; //remove the new line charecter
+int room_no;
 
 
 
 
-       //matching password and username
-        if((strcmp(roomfile,rooms)!=0 ))
+    printf("\n\n\n\n        ___________________________________________\n");
+    printf("        |             ENTER ROOM NUMBER          |\n");
+    printf("        |________________________________________|\n");
+    scanf("%d", &room_no);
 
-         {
-             system("cls");
-            printf("\n\n\n\n\n\n.........  Room is already booked  ..........\n\n\n\n\n\n");
-            printf("Try Another room \n");
-            getchar();
-            break;
 
-         }
-        else
-         {
-             system("cls");
-            printf("\n\n\n\n\n\n!!!!!!!!!  You booked this room successfully   !!!!!!!!!!!\n\n\n");
-            sleep(2);
-            system("cls");
-         }
+    FILE *file = fopen("roomsbusiness.txt","r");
+    int roomExists = 0;
+
+
+
+    if (file != NULL) {
+        int given_room_no;
+
+        while (fscanf(file,"%d",&given_room_no) == 1) {
+            if (given_room_no == room_no) {
+                roomExists = 1;
+                break;
+            }
         }
-        fclose(room);
+
+        fclose(file);
+    }
+
+    while (roomExists) {
+
+    printf("\n\n\n\n        ___________________________________\n");
+    printf("        |Room %d has already been booked. |\n",room_no);
+    printf("        |Enter a different room number     |\n");
+    printf("        |__________________________________|\n");
+        scanf("%d", &room_no);
+
+        file = fopen("roomsbusiness.txt", "r");
+        roomExists = 0;
+
+        if (file != NULL) {
+            int given_room_no;
+
+            while (fscanf(file, "%d",&given_room_no) == 1) {
+                if (given_room_no == room_no) {
+                    roomExists = 1;
+                    break;
+                }
+            }
+
+            fclose(file);
+        }
+    }
+
+    file = fopen("roomsbusiness.txt", "a");
+
+    if (file == NULL) {
+        printf("Failed to open file.\n");
+        return 0;
+    }
+
+    fprintf(file, "%d\n", room_no);
+    fclose(file);
+
+     printf("\n\n\n\n        _______________________________________\n");
+    printf("        |Room %d has been successfully booked |\n",room_no);
+    printf("        |_____________________________________|\n");
+
+    Sleep(10000);
+    system("cls");
 
     }
 
 
 void slot_economy(void)
 {
-    int number;
-   room_diagram();
-   printf("Enter the room number you want to book :");
-   scanf("%d",&number);
-}
+    int room_no;
 
 
 
 
-void room_diagram(void)
-{
-printf("           -:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::-   \n");
-printf("          .= .:::::::::::::::. :::::::::::::::: .::::::::::::::. ................  *   \n");
-printf("          .= -:              + *              + *              +.+..............+  *   \n");
-printf("          .= -:              + *              + *              +.=              +  *   \n");
-printf("          .= -:     101      + *    102       + *    103       +.=    104       +  *   \n");
-printf("          .= -:              + *              + *              +.=              +  *   \n");
-printf("          .= -=::::::::::::::+ *::::::::::::::+ *::::::::::::::+ +::::::::::::::+  *   \n");
-printf("          .=                                                                       *   \n");
-printf("          .=  +::::::::::::::= +::::::::::::::+ +::::::::::::::=.=::::::::::::::=  *   \n");
-printf("          .=  +              + *              + *              +:-              =. *   \n");
-printf("          .=  +              + *              + *              +:-              =. *   \n");
-printf("          .=  +    201       + *   202        + *    203       +:-    204       =. *   \n");
-printf("          .=  +              + *              + *              +:=              =. *   \n");
-printf("          .=  :::::::::::::::. .::::::::::::::. :::::::::::::::. ::::::::::::::::  *   \n");
-printf("          .=  +::::::::::::::= +::::::::::::::+ -=:::::::::::::+ *::::::::::::::+. *   \n");
-printf("          .=  +              + *              =.-:             + *              =. *   \n");
-printf("          .=  +    301       + *   302        =.-:   304       + *    305       =. *   \n");
-printf("          .=  +              + *              =.-:             + *              =. *   \n");
-printf("          .=  *::::::::::::::+ *::::::::::::::+ :-:::::::::::::= -::::::::::::::-  *   \n");
-printf("          .=                                                                       *   \n");
-printf("          .=  :::::::::::::::: :::::::::::::::-  =:::::::::::::=. =:::::::::::::=. *   \n");
-printf("          .=  +              + =.             =. +             -:.=             -: *   \n");
-printf("          .=  +   401        + =.  402        =. +   403       -:.=   404       -: *   \n");
-printf("          .=  +              + =.             =. +             -:.=             -: *   \n");
-printf("          .=  +::::::::::::::+ =-:::::::::::::*. =:::::::::::::=. -:::::::::::::-. *   \n");
-printf("          .=                                                                       *   \n");
-printf("           -::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::   \n");
+    printf("\n\n\n\n        ___________________________________________\n");
+    printf("        |             ENTER ROOM NUMBER          |\n");
+    printf("        |________________________________________|\n");
+    scanf("%d", &room_no);
 
-Sleep(1000);
-system("cls");
 
+    FILE *file = fopen("roomseconomy.txt","r");
+    int roomExists = 0;
+
+
+
+    if (file != NULL) {
+        int given_room_no;
+
+        while (fscanf(file,"%d",&given_room_no) == 1) {
+            if (given_room_no == room_no) {
+                roomExists = 1;
+                break;
+            }
+        }
+
+        fclose(file);
+    }
+
+    while (roomExists) {
+
+    printf("\n\n\n\n        ___________________________________\n");
+    printf("        |Room %d has already been booked. |\n",room_no);
+    printf("        |Enter a different room number     |\n");
+    printf("        |__________________________________|\n");
+        scanf("%d", &room_no);
+
+        file = fopen("roomseconomy.txt", "r");
+        roomExists = 0;
+
+        if (file != NULL) {
+            int given_room_no;
+
+            while (fscanf(file, "%d",&given_room_no) == 1) {
+                if (given_room_no == room_no) {
+                    roomExists = 1;
+                    break;
+                }
+            }
+
+            fclose(file);
+        }
+    }
+
+    file = fopen("roomseconomy.txt", "a");
+
+    if (file == NULL) {
+        printf("Failed to open file.\n");
+        return 0;
+    }
+
+    fprintf(file, "%d\n", room_no);
+    fclose(file);
+
+     printf("\n\n\n\n        _______________________________________\n");
+    printf("        |Room %d has been successfully booked |\n",room_no);
+    printf("        |_____________________________________|\n");
+
+    Sleep(10000);
+    system("cls");
 }
 
 
